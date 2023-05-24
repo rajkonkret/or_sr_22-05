111940ce41ebb8964238f9e3be2799baff2f6a1f
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-24 09:21:25 +0200

    add baza2

--- baza2.py
+++ baza2.py
@@ -13,9 +13,26 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     age = Column(Integer)
+    addresses = relationship('Address',
+                             back_populates='person',
+                             order_by="Address.email",
+                             cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'{self.name}(id={self.id})'
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    person_id = Column(ForeignKey('person.id'))
+    person = relationship('Person', back_populates='addresses')
+
+    def __str__(self):
+        return self.email
+
+    __repr__ = __str__
 
 
 Base.metadata.create_all(engine)
@@ -24,13 +41,17 @@ Session = sessionmaker(bind=engine)
 ssesion = Session()
 anakin = Person(name='Anakin', age=38)
 obi = Person(name="Obi Wan Kenobi", age=40)
+obi.addresses = [
+    Address(email='obi@exampe.com'),
+    Address(email='wanam@example.com')
+]
 
 ssesion.add(anakin)
 ssesion.add(obi)
 ssesion.commit()
 
-all = ssesion.query(Person).all()
-print(all)
+all_ = ssesion.query(Person).all()
+print(all_)
 
 an1 = ssesion.query(Person).first()
 print(an1)
@@ -42,3 +63,4 @@ obi1 = ssesion.query(Person).filter(
 ).first()
 
 print(obi1)
+print(obi1.addresses)
